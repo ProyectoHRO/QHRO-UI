24bad92fa6dc53ecf83de5396c7ba37816980dc1
Pelt06 <peltzjr11@gmail.com>
2021-08-30 22:03:13 -0600

    Add new paciente form

--- UI/DoctorForm.cs
+++ UI/DoctorForm.cs
@@ -16,5 +16,15 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
